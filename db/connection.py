06b61bb2a412f2934a61efe6fdf20c40fa74d818
Gyan-Raj7758 <bhardwaj7739@gmail.com>
2026-05-06 17:47:04 +0530

    commented the unused env values

--- db/connection.py
+++ db/connection.py
@@ -3,11 +3,11 @@ from sqlalchemy.orm import sessionmaker
 import os
 from sqlalchemy import text
 
-DB_HOST = os.getenv("DB_HOST")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
-DB_PORT = os.getenv("DB_PORT", "3306")
+# DB_HOST = os.getenv("DB_HOST")
+# DB_USER = os.getenv("DB_USER")
+# DB_PASSWORD = os.getenv("DB_PASSWORD")
+# DB_NAME = os.getenv("DB_NAME")
+# DB_PORT = os.getenv("DB_PORT", "3306")
 POSTGRESQL_NEON_URL = os.getenv("POSTGRESQL_NEON_URL")
 
 # ⚠️ Important: URL encoding for password

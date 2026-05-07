b9873574aa7512399dccd0c0ec095063370bf408
Gyan-Raj7758 <bhardwaj7739@gmail.com>
2026-05-07 10:24:38 +0530

    added missed model.py file

---
+++ ai_models/model.py
@@ -0,0 +1,10 @@
+_model = None
+
+
+def get_model():
+    global _model
+    if _model is None:
+        from sentence_transformers import SentenceTransformer
+
+        _model = SentenceTransformer("all-MiniLM-L6-v2")
+    return _model
